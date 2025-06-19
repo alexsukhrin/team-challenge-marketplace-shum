e10c5eb7afe43cbc99dcfb03f59be1065770f245
Олександр Сухрин <o.sukhryn@temabit.com>
2025-06-19 13:56:33 +0300

    Initial project commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to team-challenge/marketplace-shum
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/team_challenge/marketplace_shum_test.clj
@@ -0,0 +1,7 @@
+(ns team-challenge.marketplace-shum-test
+  (:require [clojure.test :refer :all]
+            [team-challenge.marketplace-shum :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
