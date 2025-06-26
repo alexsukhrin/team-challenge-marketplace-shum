f4938449b2f6e10f6971e82eb2750b7accd12531
Олександр Сухрин <o.sukhryn@temabit.com>
2025-06-26 14:26:37 +0300

    add fix memory

--- src/team_challenge/db.clj
+++ src/team_challenge/db.clj
@@ -3,6 +3,14 @@
             [team-challenge.config :as config]
             [datomic.api :as d]))
 
+(println "Full config:*config* =" config/*config*)
+(println ":datomic block =" (:datomic config/*config*))
+(println "DB_HOST=" (System/getenv "DB_HOST"))
+(println "DB_USER=" (System/getenv "DB_USER"))
+(println "DB_PASS=" (System/getenv "DB_PASS"))
+
 (defstate conn
-  :start (d/connect (get-in config/*config* [:datomic :db-uri]))
+  :start (do
+           (println "Datomic URI:" (get-in config/*config* [:datomic :db-uri]))
+           (d/connect (get-in config/*config* [:datomic :db-uri])))
   :stop (.release conn))
