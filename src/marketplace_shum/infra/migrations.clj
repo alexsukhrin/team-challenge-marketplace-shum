1a08373dee6495839347de28a39947232a6b06e2
Олександр Сухрин <o.sukhryn@temabit.com>
2025-07-07 22:21:46 +0300

    fix migrate

--- src/marketplace_shum/infra/migrations.clj
+++ src/marketplace_shum/infra/migrations.clj
@@ -7,22 +7,28 @@
    [marketplace-shum.infra.db :refer [db]]))
 
 (defn files []
-  (let [schemas-dir (io/resource "schemas")
-        schemas-url (io/as-url schemas-dir)]
-    (if (= "jar" (.getProtocol schemas-url))
-      (let [jar-connection (.openConnection schemas-url)
-            jar-file (-> jar-connection .getJarFileURL io/as-url .getFile)]
-        (with-open [jar (java.util.jar.JarFile. jar-file)]
-          (->> (.entries jar)
-               (filter #(and (.startsWith (.getName %) "schemas/")
-                             (.endsWith (.getName %) ".edn")))
-               (map #(.getName %)))))
-      (let [dir (io/file schemas-dir)]
-        (if (.isDirectory dir)
-          (->> (.listFiles dir)
-               (filter #(and (.isFile %) (.endsWith (.getName %) ".edn")))
-               (map #(.getPath %)))
-          [])))))
+  (let [schemas-dir (io/resource "schemas")]
+    (println "[migrations] schemas-dir:" schemas-dir)
+    (if (nil? schemas-dir)
+      (do (println "[migrations] WARNING: schemas resource not found!")
+          [])
+      (let [schemas-url (io/as-url schemas-dir)]
+        (if (= "jar" (.getProtocol schemas-url))
+          (let [jar-connection (.openConnection schemas-url)
+                jar-file (-> jar-connection .getJarFileURL io/as-url .getFile)]
+            (with-open [jar (java.util.jar.JarFile. jar-file)]
+              (->> (.entries jar)
+                   (filter #(and (.startsWith (.getName %) "schemas/")
+                                 (.endsWith (.getName %) ".edn")))
+                   (map #(.getName %)))))
+          (let [dir (io/file schemas-dir)]
+            (println "[migrations] local dir:" (.getAbsolutePath dir))
+            (if (.isDirectory dir)
+              (->> (.listFiles dir)
+                   (filter #(and (.isFile %) (.endsWith (.getName %) ".edn")))
+                   (map #(.getPath %)))
+              (do (println "[migrations] WARNING: schemas directory not found or not a directory!")
+                  []))))))))
 
 (defn load-schema [schema-path]
   (if (.startsWith schema-path "schemas/")
